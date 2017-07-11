13ce8f01550ab4060f8b270db682949683d1cf82
Towsif Khan <ankondrift@gmail.com>
2017-07-11 04:47:11 +0000

    Assignment 13

---
+++ models/Product.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ProductSchema = {
+
+  Name: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'Name required'
+  },
+
+  Discription: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'Discription required'
+
+  },
+  
+  Price: {
+    type: Number,
+    default: '',
+    trim: true,
+    required: 'Price required'
+
+  },
+
+  Quantity: {
+    type: Number,
+    default: '',
+    trim: true,
+    required: 'Quantity required'
+
+  },
+
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  },
+
+  created: {
+    type: Date,
+    default: Date.now
+  }
+}
+
+var Product = mongoose.model('Product', ProductSchema, 'products');
+module.exports = Product;
